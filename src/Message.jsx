1f5a951be3c5868edb394d544311434bb2098c79
Abdishakur Hassan Mohamed <102626624+abdishakuur366@users.noreply.github.com>
2023-03-22 14:04:57 -0700

    second

---
+++ src/Message.jsx
@@ -0,0 +1,13 @@
+import App from "./App";
+import React from "react";
+import { ReactDOM } from "react";
+function Message(){
+    let name ="Abdishakuur"
+    return
+    <div><h1>Hello {name}</h1>
+    
+    </div>
+    
+}
+
+export default Message;
---
+++ src/posts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function posts() {
+  return (
+    <div></div>
+  )
+}
+
+export default posts
